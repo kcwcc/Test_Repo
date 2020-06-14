7c7ec522c766b39013566eaca4d046caa89a4f91
kcwcc <kcwcc3@hotmail.com>
2020-06-14 15:52:10 -0400

    Moved functions to top of file

--- Test_Repo/src/main.cpp
+++ Test_Repo/src/main.cpp
@@ -12,6 +12,20 @@
 #include <Arduino.h>
 
 int16_t PWMPin = 3;
+void pwm25kHzBegin() {
+  TCCR2A = 0;                                             // TC2 Control Register A
+  TCCR2B = 0;                                             // TC2 Control Register B
+  TIMSK2 = 0;                                             // TC2 Interrupt Mask Register
+  TIFR2 = 0;                                              // TC2 Interrupt Flag Register
+  TCCR2A |= (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);  // OC2B cleared/set on match when up/down counting, fast PWM
+  TCCR2B |= (1 << WGM22) | (1 << CS21);                   // prescaler 8
+  OCR2A = 79;                                             // TOP overflow value (Hz)
+  OCR2B = 0;
+}
+
+void pwmDuty(byte ocrb) {
+  OCR2B = ocrb;                                           // PWM Width (duty)
+}
 
 void setup() {
   pinMode(PWMPin, OUTPUT);           // this is the PWM signal to the fan
@@ -32,17 +46,3 @@ void loop() {
   pwmDuty(79);                        // 100% (range = 0-79 = 1.25-100%)
   delay (5000);                       // wait
 }
-
-void pwm25kHzBegin() {
-  TCCR2A = 0;                                             // TC2 Control Register A
-  TCCR2B = 0;                                             // TC2 Control Register B
-  TIMSK2 = 0;                                             // TC2 Interrupt Mask Register
-  TIFR2 = 0;                                              // TC2 Interrupt Flag Register
-  TCCR2A |= (1 << COM2B1) | (1 << WGM21) | (1 << WGM20);  // OC2B cleared/set on match when up/down counting, fast PWM
-  TCCR2B |= (1 << WGM22) | (1 << CS21);                   // prescaler 8
-  OCR2A = 79;                                             // TOP overflow value (Hz)
-  OCR2B = 0;
-}
-
-void pwmDuty(byte ocrb) {
-  OCR2B = ocrb;                                           // PWM Width (duty)
